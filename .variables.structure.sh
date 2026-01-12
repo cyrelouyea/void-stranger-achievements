ccaf1b94f3d080c525facb723c89f7e26f766511
cyrelouyea <cyrelouyea@proton.me>
2026-01-12 19:59:59 +0100

    Add builder for bash

---
+++ .variables.structure.sh
@@ -0,0 +1,6 @@
+# Path to the UndertaleModCli executable
+UNDERTALEMODCLI_PATH=""
+
+# Path to Void Stranger's directory (The folder Steam opens when selecting Manage>Browse Local Files)
+# Could possibly be /home/USER/.local/share/Steam/steamapps/common/Void Stranger
+VOID_STRANGER_PATH=""
---
+++ builder.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ ! -f "variables.sh" ]; then
+  cp .variables.structure.sh variables.sh
+  echo "variables.sh created. Please fill in all of the empty variables, then rerun this script."
+  exit 1
+fi
+
+echo "Reading variables.sh"
+source variables.sh
+
+
+echo "--------------------------"
+echo "Merging into Void Stranger"
+echo "--------------------------"
+
+
+rm "$VOID_STRANGER_PATH/_tmp_data.win"
+"$UNDERTALEMODCLI_PATH" load "$VOID_STRANGER_PATH/clean_data.win" --scripts "patcher.csx" --output "$VOID_STRANGER_PATH/_tmp_data.win"
+rm "$VOID_STRANGER_PATH/data.win"
+cp "$VOID_STRANGER_PATH/_tmp_data.win" "$VOID_STRANGER_PATH/data.win"
+rm "$VOID_STRANGER_PATH/_tmp_data.win"
+echo All done!
